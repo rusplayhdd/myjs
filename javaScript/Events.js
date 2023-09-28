5c1af79511f58dd39d67dd72462922b2dccb7ecd
Your Name <you@example.com>
2023-09-28 08:14:56 +0300

    remade on no js in html

--- javaScript/Events.js
+++ javaScript/Events.js
@@ -1,21 +1,28 @@
-var counter = null
+document.getElementById('btn').addEventListener('click', onClickButton);
+document.getElementById('inp').addEventListener('input', input);
 
-function onClickButton(el) {
+var counter = null;
+
+function onClickButton() {
+    let btn = document.getElementById('btn');
+
     counter++;
-    el.innerHTML = "You click on the butn: " + counter;
-    el.style.background = "red";
-    el.style.color = "blue";
+    btn.innerHTML = "You click on the butn: " + counter;
+    btn.style.background = "red";
+    btn.style.color = "blue";
 
-    el.style.cssText = "border-radius: 5px; border: 5; font-size: 20px";
+    btn.style.cssText = "border-radius: 5px; border: 5; font-size: 20px";
 
     console.log(onClickButton);
 };
 
 
-function input(el) {
+function input() {
+    let el = document.getElementById('inp');
+
     if (el.value == "Hello") {
         alert("и тебе привет!")
-    } 
+    }
     else if (el.value == "hello") {
         alert("и тебе привет!")
     };
