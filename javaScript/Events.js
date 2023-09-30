7329261320fd4db405f85d4df1d63620f50422e7
rus play <nikolaenkoruslan011@gmail.com>
2023-09-30 12:10:14 +0300

    add managment lesson number 12

--- javaScript/Events.js
+++ javaScript/Events.js
@@ -1,34 +1,34 @@
-document.getElementById('btn').addEventListener('click', onClickButton);
-document.getElementById('inp').addEventListener('input', input);
+// document.getElementById('btn').addEventListener('click', onClickButton);
+// document.getElementById('inp').addEventListener('input', input);
 
-var counter = null;
+// var counter = null;
 
-function onClickButton() {
-    let btn = document.getElementById('btn');
+// function onClickButton() {
+//     let btn = document.getElementById('btn');
 
-    counter++;
-    btn.innerHTML = "You click on the butn: " + counter;
-    btn.style.background = "red";
-    btn.style.color = "blue";
+//     counter++;
+//     btn.innerHTML = "You click on the butn: " + counter;
+//     btn.style.background = "red";
+//     btn.style.color = "blue";
 
-    btn.style.cssText = "border-radius: 5px; border: 5; font-size: 20px";
+//     btn.style.cssText = "border-radius: 5px; border: 5; font-size: 20px";
 
-    console.log(onClickButton);
-};
+//     console.log(onClickButton);
+// };
 
 
-function input() {
-    let el = document.getElementById('inp');
+// function input() {
+//     let el = document.getElementById('inp');
 
-    if (el.value == "Hello") {
-        alert("и тебе привет!")
-    }
-    else if (el.value == "hello") {
-        alert("и тебе привет!")
-    };
+//     if (el.value == "Hello") {
+//         alert("и тебе привет!")
+//     }
+//     else if (el.value == "hello") {
+//         alert("и тебе привет!")
+//     };
 
-    console.log(el.value);
-}
+//     console.log(el.value);
+// }
 
 
 
---
+++ javaScript/MenagmentOfHTMLAndForm.js
@@ -0,0 +1,8 @@
+document.getElementById("form").addEventListener("submit", check)
+
+function check(event) {
+    event.preventDefault();
+    
+    console.log(event)
+ 
+}
