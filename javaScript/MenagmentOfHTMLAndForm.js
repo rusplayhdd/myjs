72dede5110e08ddf9f3b3aef57318bcfd4ad3809
rus play <nikolaenkoruslan011@gmail.com>
2023-09-30 12:37:57 +0300

    refrech

--- javaScript/MenagmentOfHTMLAndForm.js
+++ javaScript/MenagmentOfHTMLAndForm.js
@@ -1,8 +1,9 @@
+// use this methot if you don't wanna write JS code inner HTML
 document.getElementById("form").addEventListener("submit", check)
 
 function check(event) {
     event.preventDefault();
+    var el = document.getElementById("form").state;
     
-    console.log(event)
- 
+    console.log(el.value)
 }
