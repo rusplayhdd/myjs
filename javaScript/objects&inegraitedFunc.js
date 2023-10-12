0030c93f8204067c32d342f109009e3030ccf765
rus play <nikolaenkoruslan011@gmail.com>
2023-10-12 07:08:46 +0300

    refresh

--- javaScript/objects&inegraitedFunc.js
+++ javaScript/objects&inegraitedFunc.js
@@ -26,3 +26,35 @@
 // console.log(b.sort().reverse());
 // console.log(b.sort().join(", |"));
 // console.log(b.sort().join(", |").split(" "));
+
+
+
+// this example for how make classes & objects:
+// class xbox_console {
+//     constructor(color, size, CPU, GPU, memory) {
+//         this.color = color
+//         this.size = size
+//         this.CPU = CPU
+//         this.GPU = GPU
+//         this.memory = memory
+//     }
+//     info360() {
+//         console.log("Xbox 360: " + xbox_360.CPU + ", " + xbox_360.GPU +
+//             ", " + xbox_360.color); 
+//     }
+//     infoONE() {
+//         console.log("Xbox ONE: " + xbox_one.CPU + ", " + xbox_one.GPU +
+//             ", " + xbox_one.color);
+//     }
+// }
+// // this is objects:
+// let xbox_360 = new xbox_console("black", "300x240", "XCGPU", null,
+//     "512mb GDDR3");
+
+// let xbox_one = new xbox_console("white", "400x280", "8xCore 1.8_GHz",
+//     "G/CPU_juguar", "8Gb GDDR5");  
+
+// xbox_one.CPU = "8xCore_1.8_GHz";
+
+// xbox_360.info360()
+// xbox_one.infoONE()
